0e6a1210d83e7f71e39c34d211489ce8f6a83bc5
ZenithalHourlyRate <i@zenithal.me>
2021-01-08 21:45:49 +0800

    Implement Basic JS Renderer; No CSS

---
+++ static/js/index.js
@@ -0,0 +1,70 @@
+function mirrorz (json) {
+  site = json["site"]
+	$.each(json['mirrors'], function(i, e) {
+    //d = $('<div>', {text: e["cname"]});
+    id = 'mirrorz-'+e["cname"];
+    full_url = e["url"][0] == "/" ? site["url"]+e["url"] : e["url"];
+    full_help = e["help"][0] == "/" ? site["url"]+e["help"] : e["help"];
+    f = (ul) => {
+      return ul.append(
+          $('<li>').append(
+            $('<span>').append(
+              $('<a>', {"href": full_url, "text": site["abbr"]})
+            )
+          ).append(
+            $('<span>', {"text": e["desc"]})
+          ).append(
+            $('<span>').append(
+              $('<a>', {"href": full_help, "text": full_help.length > 0 ? '?' : ''})
+            )
+          ).append(
+            $('<br>')
+          ).append(
+            $('<span>').append(
+              $('<a>', {"href": e["upstream"], "text": e["upstream"]})
+            )
+          ).append(
+            $('<span>', {"text": e["status"]})
+          )
+        )
+    }
+		ul = $('#'+id);
+    if ( ul.length == 0 ) {
+      $('#mirrorz').append(
+        $('<div>', {"class": "mirrorz-div"}).append(
+          $('<h3>', {"text": e["cname"]})
+        ).append(
+          f($('<ul>', {"id": id}))
+        )
+      );
+    } else {
+      f(ul);
+    }
+	});
+}
+
+$(document).ready(() => {
+  $.each([
+    '/static/tuna/neo.json',
+    '/static/tuna/nano.json',
+    '/static/tuna/bfsu.json',
+    '/static/tuna/opentuna.json',
+  ],
+  (i, url) => {
+    $.ajax({ 
+      type: 'GET', 
+      url: url, 
+      dataType: 'json',
+      success: mirrorz,
+    });
+  });
+	$('.mirrorz-div').sort(function(a, b) {
+	  if ($('h3', a).text() < $('h3', b).text()) {
+	    return -1;
+	  } else {
+	    return 1;
+	  }
+	}).appendTo('#mirrorz');
+});
+
+// vim: sts=2 ts=2 sw=2 expandtab
