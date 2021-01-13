1590b6278d9a3d52cd8a3a0528b6326c98b716bf
Liu Xiaoyi <circuitcoder0@gmail.com>
2021-01-13 19:26:18 +0800

    Minor performance optimizations

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,6 +4,48 @@ import Icon from './Icon';
 
 const PROTO_REGEX = /(^https?:)?\/\//;
 
+const Group = React.memo(({ group, entries }) => (
+  <div class="group">
+    <h2 class="heading" id={group}>
+      <a href={`#${group}`}>
+        <Icon>link</Icon>
+      </a>
+      {group}
+    </h2>
+    {entries.map(({ full, help, upstream, desc, status, source }, idx) => (
+      <div key={idx}>
+        <a href={full}>
+          <h3>
+            {source}
+            {help && (
+              <a class="help" href={help}>
+                <Icon>help</Icon>
+              </a>
+            )}
+          </h3>
+        </a>
+        {upstream && (
+          <div class="upstream">
+            <Icon>outbound</Icon>
+            <a href={upstream}>{upstream}</a>
+          </div>
+        )}
+        {status && (
+          <div class="status">
+            <Icon>info</Icon>
+            {status}
+          </div>
+        )}
+        {desc ? (
+          <div class="desc">{desc}</div>
+        ) : (
+          <div class="desc missing">无可奉告</div>
+        )}
+      </div>
+    ))}
+  </div>
+));
+
 export default React.memo(() => {
   const [mirrors, setMirrors] = useState([]);
   const [filter, setFilter] = useState('');
@@ -46,21 +88,28 @@ export default React.memo(() => {
       mapper.get(m.cname).push(m);
     }
     return Array.from(mapper.entries())
-      .map(([k, v]) => ({ group: k, entries: v }))
+      .map(([k, v]) => ({ sortKey: k.toLowerCase(), group: k, entries: v }))
   }, [mirrors]);
 
   const updateFilter = useCallback((ev) => setFilter(ev.target.value), []);
+  const lowerFilter = filter.toLowerCase();
+  const begin = performance.now();
   const filtered = grouped
-    .filter((e) => filter === '' || e.group.indexOf(filter.toLowerCase()) !== -1)
+    .map((e) => {
+      const filtered = filter !== '' && e.sortKey.indexOf(lowerFilter) === -1
+      return { ...e, filtered };
+    })
     .sort((a, b) => {
       if(filter !== '') {
-        const aFilterDominant = a.group.toLowerCase().indexOf(filter.toLowerCase()) === 0;
-        const bFilterDominant = b.group.toLowerCase().indexOf(filter.toLowerCase()) === 0;
+        const aFilterDominant = a.sortKey.indexOf(lowerFilter) === 0;
+        const bFilterDominant = b.sortKey.indexOf(lowerFilter) === 0;
         if(aFilterDominant !== bFilterDominant) return aFilterDominant ? -1 : 1;
       }
 
-      return a.group.toLowerCase().localeCompare(b.group.toLowerCase());
+      return a.sortKey.localeCompare(b.sortKey);
     });
+  const end = performance.now();
+  console.log(`Sort`, end - begin);
 
   return (
     <main>
@@ -69,47 +118,9 @@ export default React.memo(() => {
         <Icon>search</Icon>
       </div>
 
-      {filtered.map(({ group, entries }) => (
-        <div key={group} class="group">
-          <h2 class="heading" id={group}>
-            <a href={`#${group}`}>
-              <Icon>link</Icon>
-            </a>
-            {group}
-          </h2>
-          {entries.map(
-            ({ full, help, upstream, desc, status, source }, idx) => (
-              <div key={idx}>
-                <a href={full}>
-                  <h3>
-                    {source}
-                    {help && (
-                      <a class="help" href={help}>
-                        <Icon>help</Icon>
-                      </a>
-                    )}
-                  </h3>
-                </a>
-                {upstream && (
-                  <div class="upstream">
-                    <Icon>outbound</Icon>
-                    <a href={upstream}>{upstream}</a>
-                  </div>
-                )}
-                {status && (
-                  <div class="status">
-                    <Icon>info</Icon>
-                    {status}
-                  </div>
-                )}
-                {desc ? (
-                  <div class="desc">{desc}</div>
-                ) : (
-                  <div class="desc missing">无可奉告</div>
-                )}
-              </div>
-            )
-          )}
+      {filtered.map(({ group, entries, filtered }) => (
+        <div class={filtered ? "filtered" : ""} key={group}>
+          <Group group={group} entries={entries} />
         </div>
       ))}
     </main>
