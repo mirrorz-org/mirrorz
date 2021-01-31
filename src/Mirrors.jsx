083128854d4540af9f6c0cf530d66b13f90c082f
XYenon <i@xyenon.bid>
2021-01-31 21:56:58 +0800

    feat: add route

--- src/Mirrors.jsx
+++ src/Mirrors.jsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useState, useMemo, useCallback } from "react";
+import React, { useState, useEffect, useMemo, useCallback } from "react";
+import {
+  Link,
+  useLocation,
+  useRouteMatch,
+} from "react-router-dom";
+import { Element, scroller } from 'react-scroll';
 import Icon from './Icon';
 
 const STATUS_MAPPING = {
@@ -8,19 +14,19 @@ const STATUS_MAPPING = {
   F: 'Failed',
   N: 'New',
   U: 'Unknown',
-}
+};
 
-const Group = React.memo(({ group, entries, filtered }) => {
-  const [collapse, setCollapse] = useState(true);
+const Group = React.memo(({ group, entries, filtered, defaultCollapse = true }) => {
+  const [collapse, setCollapse] = useState(defaultCollapse);
   const toggleCollapse = useCallback(() => setCollapse(c => !c), []);
 
   const summary = useMemo(() => {
     const mapper = new Map();
-    entries.map(({status}) => {
+    entries.map(({ status }) => {
       [...status].map((s) => {
-        if(!mapper.has(s)) mapper.set(s, 0);
+        if (!mapper.has(s)) mapper.set(s, 0);
         mapper.set(s, mapper.get(s) + 1);
-      })
+      });
     });
     return (
       <h2 className="summary">
@@ -58,20 +64,24 @@ const Group = React.memo(({ group, entries, filtered }) => {
     )
   }, [entries]);
 
+  let match = useRouteMatch();
+
   return (
     <div className={"group" + (filtered ? " filtered" : "") + (collapse ? "" : " group-expanded")}>
       <div className="group-header" id={group} onClick={toggleCollapse}>
-        <h2 className="heading">
-          <a href={`#${group}`}>
-            {collapse ?
-            (<Icon>add</Icon>):
-            (<Icon>remove</Icon>)
-            }
-          </a>
-          {group}
-        </h2>
+        <Element name={group}>
+          <h2 className="heading">
+            <Link to={`${match.url}#${group}`}>
+              {collapse ?
+                (<Icon>add</Icon>) :
+                (<Icon>remove</Icon>)
+              }
+            </Link>
+            {group}
+          </h2>
+        </Element>
         <div>
-            {summary}
+          {summary}
         </div>
       </div>
       <div className="group-items">
@@ -97,15 +107,15 @@ const Group = React.memo(({ group, entries, filtered }) => {
               <div className="status">
                 <Icon>info</Icon>
                 {[...status].map((s) => {
-                    return STATUS_MAPPING[s];
+                  return STATUS_MAPPING[s];
                 }).join("+") ?? "Unknown"}
               </div>
             )}
             {desc ? (
               <div className="desc">{desc}</div>
             ) : (
-              <div className="desc missing">无可奉告</div>
-            )}
+                <div className="desc missing">无可奉告</div>
+              )}
           </div>
         ))}
       </div>
@@ -115,12 +125,13 @@ const Group = React.memo(({ group, entries, filtered }) => {
 
 export default React.memo(({ mirrors }) => {
   const [filter, setFilter] = useState('');
+  const [unfolded, setUnfolded] = useState(null);
 
   // Clustering
   const grouped = useMemo(() => {
     const mapper = new Map();
-    for(const m of mirrors) {
-      if(!mapper.has(m.cname)) mapper.set(m.cname, []);
+    for (const m of mirrors) {
+      if (!mapper.has(m.cname)) mapper.set(m.cname, []);
       mapper.get(m.cname).push(m);
     }
     return Array.from(mapper.entries())
@@ -133,19 +144,30 @@ export default React.memo(({ mirrors }) => {
   const filtered = grouped
     .map((e) => {
       const filtered = filter !== '' && e.sortKey.indexOf(lowerFilter) === -1
-      return { ...e, filtered };
+      return { ...e, filtered, defaultCollapse: unfolded !== e.group };
     })
     .sort((a, b) => {
-      if(filter !== '') {
+      if (filter !== '') {
         const aFilterDominant = a.sortKey.indexOf(lowerFilter) === 0;
         const bFilterDominant = b.sortKey.indexOf(lowerFilter) === 0;
-        if(aFilterDominant !== bFilterDominant) return aFilterDominant ? -1 : 1;
+        if (aFilterDominant !== bFilterDominant) return aFilterDominant ? -1 : 1;
       }
 
       return a.sortKey.localeCompare(b.sortKey);
     });
   const end = performance.now();
   console.log(`Sort`, end - begin);
+
+  const location = useLocation();
+  useEffect(() => {
+    const group = location.hash.slice(1);
+    setUnfolded(group);
+    scroller.scrollTo(group, {
+      duration: 500,
+      smooth: true,
+      offset: -100,
+    });
+  }, [location, filtered]);
 
   return (
     <div className={"mirrorz"}>
@@ -155,9 +177,9 @@ export default React.memo(({ mirrors }) => {
       </div>
 
       <div className="mirrors">
-      {filtered.map(({ group, entries, filtered }) => (
-          <Group key={group} filtered={filtered} group={group} entries={entries} />
-      ))}
+        {filtered.map(({ group, entries, filtered, defaultCollapse }) => (
+          <Group key={group} filtered={filtered} group={group} entries={entries} defaultCollapse={defaultCollapse} />
+        ))}
       </div>
     </div>
   );
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState, useMemo, useCallback } from "react";
+import React, { useEffect, useState } from "react";
+import { HashRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 import MIRROR_URLS from "./config/mirrors";
-import Icon from './Icon';
-import Mirrors from './Mirrors';
-import ISO from './ISO';
-import Site from './Site';
+import Mirrors from "./Mirrors";
+import ISO from "./ISO";
+import Site from "./Site";
 
 const PROTO_REGEX = /(^https?:)?\/\//;
 
+// eslint-disable-next-line react/display-name
 export default React.memo(() => {
   const [mirrors, setMirrors] = useState([]);
   const [isoinfo, setIsoinfo] = useState([]);
   const [site, setSite] = useState([]);
-  const [page, setPage] = useState(1);
 
   // Load all mirror configurations
   useEffect(() => {
@@ -19,61 +19,78 @@ export default React.memo(() => {
       const resp = await fetch(url);
       const { site, info, mirrors } = await resp.json();
 
-      const parsed = mirrors.map(({ cname, url, help, abbr, desc, upstream, status }) => {
-        const fullUrl = url.match(PROTO_REGEX) ? url : site.url + url;
-        const helpUrl =
-          help === "" ? null : help.match(PROTO_REGEX) ? help : site.url + help;
-        return {
-          cname,
-          full: fullUrl,
-          help: helpUrl,
-          upstream,
-          desc,
-          status,
-          source: site.abbr,
-        };
-      });
-      setMirrors(original => original.concat(parsed));
+      const parsed = mirrors.map(
+        ({ cname, url, help, abbr, desc, upstream, status }) => {
+          const fullUrl = url.match(PROTO_REGEX) ? url : site.url + url;
+          const helpUrl =
+            help === ""
+              ? null
+              : help.match(PROTO_REGEX)
+              ? help
+              : site.url + help;
+          return {
+            cname,
+            full: fullUrl,
+            help: helpUrl,
+            upstream,
+            desc,
+            status,
+            source: site.abbr,
+          };
+        }
+      );
+      setMirrors((original) => original.concat(parsed));
 
-      setSite(original => original.concat([{ site, parsed }]));
+      setSite((original) => original.concat([{ site, parsed }]));
 
       const fullinfo = info.map(({ category, distro, urls }) => {
         const fullUrls = urls.map(({ name, url }) => {
           return {
             name: name + " [" + site.abbr + "]",
-            url: url.match(PROTO_REGEX) ? url: site.url + url,
-          }
-        })
+            url: url.match(PROTO_REGEX) ? url : site.url + url,
+          };
+        });
         return {
           category,
           distro,
           urls: fullUrls,
         };
       });
-      setIsoinfo(original => original.concat(fullinfo));
+      setIsoinfo((original) => original.concat(fullinfo));
     }
 
     // Fires and forget
     for (const mirror of MIRROR_URLS) initMirror(mirror);
   }, []);
 
-  // sidebar funcs
-  const toISO = useCallback(() => setPage(1), []);
-  const toList = useCallback(() => setPage(2), []);
-  const toSite = useCallback(() => setPage(3), []);
-
   return (
-    <div>
-      <div className="sidebar">
-        <h2 className={page == 1? "active": ""} onClick={toISO}>ISO</h2>
-        <h2 className={page == 2? "active": ""} onClick={toList}>List</h2>
-        <h2 className={page == 3? "active": ""} onClick={toSite}>Site</h2>
+    <Router>
+      <div>
+        <div className="sidebar">
+          <NavLink exact to="/" activeClassName="active">
+            <h2>ISO</h2>
+          </NavLink>
+          <NavLink to="/list" activeClassName="active">
+            <h2>List</h2>
+          </NavLink>
+          <NavLink to="/site" activeClassName="active">
+            <h2>Site</h2>
+          </NavLink>
+        </div>
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <ISO isoinfo={isoinfo} />
+            </Route>
+            <Route path="/list">
+              <Mirrors mirrors={mirrors} />
+            </Route>
+            <Route path="/site">
+              <Site site={site} />
+            </Route>
+          </Switch>
+        </main>
       </div>
-      <main>
-        {page == 1 && <ISO isoinfo={isoinfo}/>}
-        {page == 2 && <Mirrors mirrors={mirrors}/>}
-        {page == 3 && <Site site={site}/>}
-      </main>
-    </div>
+    </Router>
   );
 });
