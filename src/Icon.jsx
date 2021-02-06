87f5c02575e702ee5eb8c3057ebcd3c2c6b8175b
ZenithalHourlyRate <i@zenithal.me>
2021-02-06 23:14:31 +0800

    Use modern picture tag in dark/light mode logo switch

--- src/Icon.jsx
+++ src/Icon.jsx
@@ -3,15 +3,20 @@ import React from 'react';
 export default React.memo(({ children }) => <i className="material-icons">{children}</i>);
 
 export const Logo = React.memo(({ site, className }) => {
+  let logo_darkmode = null;
   let logo = null;
-  // https://stackoverflow.com/questions/56393880/how-do-i-detect-dark-mode-using-javascript
-  // dark mode and has dark logo
-  if (site.logo_darkmode && site.logo_darkmode !== ""  && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    logo = site.logo_darkmode;
-  } else if (site.logo && site.logo !== "") {
+  if (site.logo_darkmode && site.logo_darkmode !== "") {
+    logo_darkmode = site.logo_darkmode;
+  }
+  if (site.logo && site.logo !== "") {
     logo = site.logo;
   }
 
   if (logo !== null)
-    return (<img src={logo} title={site.abbr} className={className} />);
+    return (
+      <picture>
+        {logo_darkmode && (<source srcset={logo_darkmode} media="(prefers-color-scheme: dark)" />)}
+        <img src={logo} title={site.abbr} className={className} />
+      </picture>
+    );
 });
