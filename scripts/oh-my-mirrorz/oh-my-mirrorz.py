d3d08c63eb481672ace65b5a4b19a8b8ee1c9fe6
abc1763613206 <abc1763613206@163.com>
2021-02-09 09:11:36 +0800

    fix: code style

--- scripts/oh-my-mirrorz/oh-my-mirrorz.py
+++ scripts/oh-my-mirrorz/oh-my-mirrorz.py
@@ -41,10 +41,10 @@ def site_info(url):
     return requests.get(url, timeout=10).json()
 
 def speed_test(url):
+    null = '/dev/null'
     if (sys.platform.startswith('win32')):
-        res = subprocess.run(['curl', '-qso', 'NUL', '-w', '%{http_code} %{speed_download}', '-m5', url], stdout=subprocess.PIPE)
-    else:
-        res = subprocess.run(['curl', '-qso', '/dev/null', '-w', '%{http_code} %{speed_download}', '-m5', url], stdout=subprocess.PIPE)
+        null = 'NUL'
+    res = subprocess.run(['curl', '-qso', null, '-w', '%{http_code} %{speed_download}', '-m5', url], stdout=subprocess.PIPE)
     code, speed = res.stdout.decode('utf-8').split()
     return int(code), float(speed)
 
