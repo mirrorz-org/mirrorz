3162ec546d10f959836a4502ac743f38fdbbbfea
ZenithalHourlyRate <i@zenithal.me>
2021-02-19 23:12:51 +0800

    Use non-ipv6 source of tunasync for nano/neo

--- src/parser/nano.js
+++ src/parser/nano.js
@@ -7,12 +7,12 @@ import disk from "./disk";
 export default async function () {
   const name_func = await cname();
   const site = await (await fetch("/static/json/site/nano.json")).json();
-  site["disk"] = await disk("https://nanomirrors.tuna.tsinghua.edu.cn/static/status/disk.json")
+  site["disk"] = await disk("https://mirrors.tuna.tsinghua.edu.cn/static/status/nano/disk.json")
 
-  let mirrors = await tunasync("https://nanomirrors.tuna.tsinghua.edu.cn/static/tunasync.json");
-  mirrors = await options("https://nanomirrors.tuna.tsinghua.edu.cn/static/js/options.json", mirrors);
+  let mirrors = await tunasync("https://mirrors.tuna.tsinghua.edu.cn/static/tunasync.json.nano");
+  mirrors = await options("https://mirrors.tuna.tsinghua.edu.cn/static/js/options.json", mirrors);
 
-  info = await isoinfo("https://nanomirrors.tuna.tsinghua.edu.cn/static/status/isoinfo.json");
+  info = await isoinfo("https://mirrors.tuna.tsinghua.edu.cn/static/status/nano/isoinfo.json");
 
   return {
     site,
--- src/parser/neo.js
+++ src/parser/neo.js
@@ -6,13 +6,13 @@ import disk from "./disk";
 
 export default async function () {
   const name_func = await cname();
-  const site = await (await fetch("/static/json/site/neo.json")).json();
-  site["disk"] = await disk("https://neomirrors.tuna.tsinghua.edu.cn/static/status/disk.json")
+  const site = await (await fetch("/static/json/site/nano.json")).json();
+  site["disk"] = await disk("https://mirrors.tuna.tsinghua.edu.cn/static/status/neo/disk.json")
 
-  let mirrors = await tunasync("https://neomirrors.tuna.tsinghua.edu.cn/static/tunasync.json");
-  mirrors = await options("https://neomirrors.tuna.tsinghua.edu.cn/static/js/options.json", mirrors);
+  let mirrors = await tunasync("https://mirrors.tuna.tsinghua.edu.cn/static/tunasync.json.neo");
+  mirrors = await options("https://mirrors.tuna.tsinghua.edu.cn/static/js/options.json", mirrors);
 
-  info = await isoinfo("https://neomirrors.tuna.tsinghua.edu.cn/static/status/isoinfo.json");
+  info = await isoinfo("https://mirrors.tuna.tsinghua.edu.cn/static/status/neo/isoinfo.json");
 
   return {
     site,
