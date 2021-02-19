38146b0aa485949d0818eb38d0f29055f8995ecb
ZenithalHourlyRate <i@zenithal.me>
2021-02-19 21:04:53 +0800

    Add legacy/static json for frontend generated json

--- scripts/oh-my-mirrorz/oh-my-mirrorz.py
+++ scripts/oh-my-mirrorz/oh-my-mirrorz.py
@@ -15,22 +15,25 @@ big = {
 }
 
 mirrors = [
-    "https://status.tuna.wiki/mirrorz/static/neo.json",
-    "https://status.tuna.wiki/mirrorz/static/nano.json",
-    "https://status.tuna.wiki/mirrorz/static/opentuna.json",
-    "https://status.tuna.wiki/mirrorz/static/bfsu.json",
+    "https://mirrorz.org/static/json/legacy/tuna.json",
+    "https://mirrorz.org/static/json/legacy/opentuna.json",
+    "https://mirrorz.org/static/json/legacy/bfsu.json",
     "https://mirrors.ustc.edu.cn/static/json/mirrorz.json",
     "https://mirror.sjtu.edu.cn/mirrorz/siyuan.json",
     "https://mirror.sjtu.edu.cn/mirrorz/zhiyuan.json",
-    "https://mirrors.hit.edu.cn/static/mirrorz.json",
     "https://mirrors.dgut.edu.cn/static/mirrorz.json",
     "https://mirrors.sustech.edu.cn/mirrorz/mirrorz.json",
-    "https://iptv.uestc.edu.cn/mirrors/mirrorz.json",
     "https://mirrorz.org/static/tmp/bjtu.json",
     "https://mirrorz.org/static/tmp/njupt.json",
-    "https://mirrorz.org/static/tmp/cqu.json",
     "https://mirrorz.org/static/tmp/xjtu.json",
     "https://mirrorz.org/static/tmp/tongji.json",
+    "https://mirrorz.org/static/json/legacy/cqu.json",
+    "https://mirrorz.org/static/json/legacy/uestc.json",
+    "https://mirrorz.org/static/json/legacy/hit.json",
+    "https://mirrorz.org/static/json/legacy/nju.json",
+    "https://mirrorz.org/static/json/legacy/hust.json",
+    "https://mirrorz.org/static/json/legacy/neusoft.json",
+    "https://mirrorz.org/static/json/legacy/lzu.json",
 ]
 
 map = {}
