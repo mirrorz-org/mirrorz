8b4b4dddea149520b321930cbdaab8f48807826f
ZenithalHourlyRate <i@zenithal.me>
2021-02-19 23:45:22 +0800

    Fix two nano

--- src/parser/neo.js
+++ src/parser/neo.js
@@ -6,7 +6,7 @@ import disk from "./disk";
 
 export default async function () {
   const name_func = await cname();
-  const site = await (await fetch("/static/json/site/nano.json")).json();
+  const site = await (await fetch("/static/json/site/neo.json")).json();
   site["disk"] = await disk("https://mirrors.tuna.tsinghua.edu.cn/static/status/neo/disk.json")
 
   let mirrors = await tunasync("https://mirrors.tuna.tsinghua.edu.cn/static/tunasync.json.neo");
