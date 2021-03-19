4678147ad93a58374ef9919aed00c4bf046ffe69
Zenithal <i@zenithal.me>
2021-03-19 16:53:06 +0800

    Parser: pku add disk usage

--- src/parser/pku.js
+++ src/parser/pku.js
@@ -16,9 +16,22 @@ const statusConverter = function(item) {
     return c + "O" + last + "X" + next;
 };
 
+const human = function(size) {
+  const scale = ["GiB", "TiB", "PiB"];
+  let i = 0;
+  while (size > 1024) {
+    size /= 1024;
+    i += 1;
+  }
+  return size.toFixed(2) + scale[i];
+}
+
 module.exports = async function () {
   const name_func = await cname();
   const site = await (await fetch("https://mirrorz.org/static/json/site/pku.json")).json();
+  const disk = await (await fetch("https://mirrors.pku.edu.cn/monitor_device/api/v1/data?chart=disk_space._data&after=-1&before=0&points=1&group=average&gtime=0&format=json&options=seconds&options=jsonwrap")).json();
+  site.disk = human(disk.latest_values[1]) + "/" + human(disk.latest_values[0] + disk.latest_values[1]);
+
   const stat = await (await fetch("https://mirrors.pku.edu.cn/monitor/status")).json();
 
   const mirrors = stat.map((item) => {
