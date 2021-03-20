9322a84bd4de97df39e5552f52b92a3eb740a0b7
ZenithalHourlyRate <i@zenithal.me>
2021-03-21 00:30:38 +0800

    Update oh-my-mirrorz build process
    Make mirrors.js config build by CI, not by hand

--- scripts/oh-my-mirrorz/oh-my-mirrorz.py
+++ scripts/oh-my-mirrorz/oh-my-mirrorz.py
@@ -18,35 +18,8 @@ big = {
     'debian': '/ls-lR.gz',
 }
 
-mirrors = [
-    "https://mirrorz.org/static/json/legacy/tuna.json",
-    "https://mirrorz.org/static/json/legacy/opentuna.json",
-    "https://mirrorz.org/static/json/legacy/bfsu.json",
-    "https://mirrors.ustc.edu.cn/static/json/mirrorz.json",
-    "https://mirror.sjtu.edu.cn/mirrorz/siyuan.json",
-    "https://mirror.sjtu.edu.cn/mirrorz/zhiyuan.json",
-    "https://mirrors.dgut.edu.cn/static/mirrorz.json",
-    "https://mirrors.sustech.edu.cn/mirrorz/mirrorz.json",
-    "https://mirrors.nwafu.edu.cn/api/mirrorz/info.json",
-    "https://mirrorz.org/static/tmp/bjtu.json",
-    "https://mirrorz.org/static/tmp/njupt.json",
-    "https://mirrorz.org/static/tmp/xjtu.json",
-    "https://mirrorz.org/static/tmp/tongji.json",
-    "https://mirrorz.org/static/json/legacy/cqu.json",
-    "https://mirrorz.org/static/json/legacy/uestc.json",
-    "https://mirrorz.org/static/json/legacy/hit.json",
-    "https://mirrorz.org/static/json/legacy/nju.json",
-    "https://mirrorz.org/static/json/legacy/hust.json",
-    "https://mirrorz.org/static/json/legacy/neusoft.json",
-    "https://mirrorz.org/static/json/legacy/lzu.json",
-    "https://mirrorz.org/static/json/legacy/neu.json",
-    "https://mirrorz.org/static/json/legacy/nyist.json",
-    "https://mirrorz.org/static/json/legacy/scau.json",
-    "https://mirrorz.org/static/json/legacy/zju.json",
-    "https://mirrorz.org/static/json/legacy/pku.json",
-    "https://mirrorz.org/static/json/legacy/byrio.json",
-    "https://mirrorz.org/static/json/legacy/cqupt.json",
-]
+# filled by CI
+mirrors = []
 
 map = {}
 
