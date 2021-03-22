dd1863f6e3381dd2add82a0d6213e607028e834b
Yifan Bian <bianyifan@users.noreply.github.com>
2021-03-22 16:57:28 +0800

    Extract data loading and transformation to custom hooks

--- src/ui/Root.tsx
+++ src/ui/Root.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useMemo } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,93 +11,19 @@ import Site from "./Site";
 import About from "./About";
 import Debug from "./Debug";
 import Monitor from "./Monitor";
-import UPSTREAM_LIST from "../config/upstream";
-import { Mirrorz } from "../schema";
-import { Parser } from "../parser";
-
-const PROTO_REGEX = /(^https?:)?\/\//;
+import { useIsoInfo, useMirrors, useMirrorzSites, useSites } from "./hooks";
 
 // eslint-disable-next-line react/display-name
 export default React.memo(() => {
-  const [mirrorz, setMirrorz] = useState(new Map());
+  const mirrorz = useMirrorzSites();
+  const site = useSites(mirrorz);
+  const mirrors = useMirrors(mirrorz);
+  const isoinfo = useIsoInfo(mirrorz);
 
-  const [mirrors, setMirrors] = useState(new Map());
-  const [isoinfo, setIsoinfo] = useState(new Map());
-  const [site, setSite] = useState(new Map());
-
-  const mirrorzList = useMemo(() => Array.from(mirrorz.values()), [mirrorz]);
-  const mirrorsList = useMemo(() => Array.from(mirrors.values()).flat(), [mirrors]);
-  const isoinfoList = useMemo(() => Array.from(isoinfo.values()).sort((a, b) => a.site.abbr.localeCompare(b.site.abbr)), [isoinfo]);
-  const siteList = useMemo(() => Array.from(site.values()).sort((a, b) => a.site.abbr.localeCompare(b.site.abbr)), [site]);
-
-  // Load all mirror configurations
-  useEffect(() => {
-    async function initMirror(url: string | Parser) {
-      let obj: Mirrorz;
-      if (typeof (url) == "string") {
-        const resp = await fetch(url);
-        obj = await resp.json();
-      } else {
-        obj = await url();
-      }
-      setMirrorz((original) => new Map(original.set(url, obj)));
-      const { site, info, mirrors } = obj;
-
-      const parsed = mirrors.map(
-        ({ cname, url, help, size, desc, upstream, status }) => {
-          const fullUrl = url.match(PROTO_REGEX) ? url : site.url + url;
-          const helpUrl =
-            (help === "" || help === undefined || help === null)
-              ? null
-              : help.match(PROTO_REGEX)
-                ? help
-                : site.url + help;
-          return {
-            cname,
-            full: fullUrl,
-            help: helpUrl,
-            upstream,
-            desc,
-            status: status === undefined ? "U" : status,
-            size,
-            source: site.abbr,
-            note: site.note,
-          };
-        }
-      );
-      setMirrors((original) => new Map(original.set(url, parsed)));
-
-      setSite((original) => new Map(original.set(url, { site, parsed })));
-
-      const fullinfo = info.map(({ category, distro, urls }) => {
-        const fullUrls = urls.map(({ name, url }) => {
-          return {
-            name: name,
-            url: url.match(PROTO_REGEX) ? url : site.url + url,
-          };
-        });
-        return {
-          category,
-          distro,
-          urls: fullUrls,
-        };
-      });
-      setIsoinfo((original) => new Map(original.set(url, { site, info: fullinfo })));
-    }
-
-    // Fires
-    for (const mirror of UPSTREAM_LIST) initMirror(mirror);
-
-    const interval = setInterval(() => {
-      console.log("Page", document.visibilityState);
-      if (document.visibilityState === "visible") {
-        console.log("Refresh data");
-        for (const mirror of UPSTREAM_LIST) initMirror(mirror);
-      }
-    }, 30 * 1000);
-
-    return () => clearInterval(interval);
-  }, []);
+  const mirrorzList = useMemo(() => Object.values(mirrorz), [mirrorz]);
+  const mirrorsList = useMemo(() => Object.values(mirrors).flat(), [mirrors]);
+  const isoinfoList = useMemo(() => Object.values(isoinfo).sort((a, b) => a.site.abbr.localeCompare(b.site.abbr)), [isoinfo]);
+  const siteList = useMemo(() => Object.values(site).sort((a, b) => a.site.abbr.localeCompare(b.site.abbr)), [site]);
 
   return (
     <Router>
