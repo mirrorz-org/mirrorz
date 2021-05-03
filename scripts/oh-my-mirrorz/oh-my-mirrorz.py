3c9bcf766d2be355a5d4e9249ec9700a20a90bb3
Zenithal <i@zenithal.me>
2021-05-03 20:06:13 +0800

    Update big list in oh-my-mirrorz

--- scripts/oh-my-mirrorz/oh-my-mirrorz.py
+++ scripts/oh-my-mirrorz/oh-my-mirrorz.py
@@ -13,7 +13,7 @@ CURL_VERSION = ''
 
 big = {
     'centos': '/8/isos/x86_64/CentOS-8.3.2011-x86_64-dvd1.iso',
-    'archlinux': '/iso/latest/archlinux-2021.03.01-x86_64.iso',
+    'archlinux': '/iso/latest/archlinux-2021.05.01-x86_64.iso',
     'ubuntu': '/indices/md5sums.gz',
     'debian': '/ls-lR.gz',
 }
